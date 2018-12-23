5ca36e65dc26ef78ee53bb7134a274fc306b56a7
Benjamin Jones <bljones36@gmail.com>
2018-12-23 15:20:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/new' do
+
+    erb :create_puppy
+  end
+
   post '/puppy' do
     @puppy = Puppy.new(params[:name],params[:breed],params[:age])
     erb :display_puppy
